29013482e4d2ccbe36bca2e6cb53dfa5b9d4f716
Khushi Gudepu <moppets@Khushis-MacBook-Air.local>
2025-09-02 18:16:04 -0400

    Fix alias config for @ imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import { cloudflare } from "@cloudflare/vite-plugin"
-import { fileURLToPath, URL } from "url"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url))
-    }
+      "@": path.resolve(__dirname, "src"),
+    },
   },
-  plugins: [react(), cloudflare()]
-})
+});
+
